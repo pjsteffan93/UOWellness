e78c3188df7a64a5e026583cf61bbfc163fab1e7
Paul Steffan <pjsteffan93@gmail.com>
2025-02-09 16:10:40 -0800

    reverting logger

--- frontend/fundamentals.py
+++ frontend/fundamentals.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import streamlit as st
 
 import streamlit as st
-from openai import AssistantEventHandler, OpenAI
+from openai import OpenAI
 from openai.types.beta.assistant_stream_event import ThreadMessageDelta
 from openai.types.beta.threads.text_delta_block import TextDeltaBlock 
 import os
@@ -23,31 +23,7 @@ def get_asst_by_name(client,name):
 client = OpenAI(api_key=OPENAI_API_KEY)
 assistant = client.beta.assistants.retrieve(assistant_id=get_asst_by_name(client,"Puddles_v2"))
 
-class EventHandler(AssistantEventHandler):
-    @override
-    def on_text_created(self, text) -> None:
-        print(f"\nassistant > ", end="", flush=True)
 
-    @override
-    def on_tool_call_created(self, tool_call):
-        print(f"\nassistant > {tool_call.type}\n", flush=True)
-
-    @override
-    def on_message_done(self, message) -> None:
-        # print a citation to the file searched
-        message_content = message.content[0].text
-        annotations = message_content.annotations
-        citations = []
-        for index, annotation in enumerate(annotations):
-            message_content.value = message_content.value.replace(
-                annotation.text, f"[{index}]"
-            )
-            if file_citation := getattr(annotation, "file_citation", None):
-                cited_file = client.files.retrieve(file_citation.file_id)
-                citations.append(f"[{index}] {cited_file.filename}")
-
-        print(message_content.value)
-        print("\n".join(citations))
 
 
 
